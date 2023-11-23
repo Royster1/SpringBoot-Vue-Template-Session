1446026458193e7df2d66b4e70c8450b942027a7
Wzl <2318448707@qq.com>
2023-11-24 01:04:33 +0800

    前端:基本路由配置

---
+++ study-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
+    <div style="flex: 1">
+      <el-image style="width: 100%;height: 100%;" fit="cover"
+                src="http://img.netbian.com/file/2023/0714/191543TnXnr.jpg"></el-image>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里你可以学习Java整套系列, 如何搭建网站</div>
+      <div style="margin-top: 5px">0基础也能轻松入门Java</div>
+    </div>
+    <div style="width: 400px;background-color: white;">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.welcome-title{
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+</style>
